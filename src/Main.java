869920e1cc7933303f09de977b2cfe944bc902b3
Vedant Patel <100173804@lubbockisd.net>
2023-08-31 09:31:49 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("    ___\n" +
+                "     o|* *|o\n" +
+                "     o|* *|o\n" +
+                "     o|* *|o\n" +
+                "      \\===/         ");
+        System.out.print(
+                "       |||\n" +
+                "       |||\n" +
+                "       |||\n");
+        System.out.print(
+                "    ___|||___\n" +
+                "   /   |||   \\\n" +
+                "  /    |||    \\\n" +
+                " |     |||     |\n" +
+                "  \\   (|||)   /");
+        System.out.print(" |   |||   |\n" +
+                "  /    |||    \\\n" +
+                " /     |||     \\\n" +
+                "/      |||      \\\n" +
+                "|     [===]     |");
+        System.out.print(" \\             /\n" +
+                "  '.         .'\n" +
+                "    '-------'\n");
+
+        System.out.print(" credits to ASII art archive ");
+    }
+}
